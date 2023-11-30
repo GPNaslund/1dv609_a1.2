da9697b24087ce25d8fcd0a52495b2bbbf32422e
GPNaslund <135686203+GPNaslund@users.noreply.github.com>
2023-11-30 17:25:14 +0100

    GREEN

--- TaskManager/src/model/Task.cs
+++ TaskManager/src/model/Task.cs
@@ -17,6 +17,7 @@ namespace TaskManager.src.model
 
         private string ValidateName(string name)
         {
+            ArgumentNullException.ThrowIfNull(name);
             if (string.IsNullOrWhiteSpace(name))
             {
                 throw new ArgumentException("A tasks name cannot be empty!");
