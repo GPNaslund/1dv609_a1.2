d9c44df1610bcbca569826ffe312e8ca50afcc9e
GPNaslund <135686203+GPNaslund@users.noreply.github.com>
2023-11-30 17:09:34 +0100

    GREEN

--- TaskManager/src/model/Task.cs
+++ TaskManager/src/model/Task.cs
@@ -1,7 +1,12 @@
+using System.Reflection.Metadata.Ecma335;
+
 namespace TaskManager.src.model
 {
-    public class Task(string name, string description, DateTime dueDate)
+    public class Task
     {
-        
+        public Task(string name, string description, DateTime dueDate)
+        {
+            throw new ArgumentException();
+        }
     }
 }
