53404bc56835f17c6f019e94a1f08c422c48a40b
GPNaslund <135686203+GPNaslund@users.noreply.github.com>
2023-12-01 09:15:41 +0100

    RED

--- TaskManager.Tests/UnitTests/controller/ListTasksControllerTest.cs
+++ TaskManager.Tests/UnitTests/controller/ListTasksControllerTest.cs
@@ -27,7 +27,20 @@ namespace TaskManager.Tests.UnitTests.controller
 
             MockView.Verify(obj => obj.DisplayHeader(), Times.AtLeastOnce());
             MockView.Verify(obj => obj.DisplayMenu(), Times.AtLeastOnce());
+        }
 
+        [Fact]
+        public void Initialize_ShouldCallServiceWithListByCommand_BasedOnUserInput()
+        {
+            Mock<View> MockView = new Mock<View>();
+            Mock<ITaskService> MockTaskService = new Mock<ITaskService>();
+            MockView.Setup(obj => obj.GetInput(It.IsAny<string>())).Returns("1");
+
+            ListTasksController Sut = new(MockView.Object, MockTaskService.Object);
+
+            Sut.Initialize();
+
+            MockTaskService.Verify(obj => obj.ListTasksBy(ListByCommand.List_By_Due_Date));
         }
     }
 }
