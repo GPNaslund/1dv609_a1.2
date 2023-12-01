329e256aba5d923fda77fd39fbdce8a94103c9dc
GPNaslund <135686203+GPNaslund@users.noreply.github.com>
2023-12-01 06:06:22 +0100

    GREEN

--- TaskManager/src/controller/MainMenuController.cs
+++ TaskManager/src/controller/MainMenuController.cs
@@ -4,14 +4,16 @@ namespace TaskManager.src.controller
 {
     public class MainMenuController
     {
+        private readonly View View;
         public MainMenuController(View view)
         {
             ArgumentNullException.ThrowIfNull(view);
+            View = view;
         }
 
         public void Initialize()
         {
-            
+            View.DisplayMenu();
         }
     }
 }
