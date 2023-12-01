155c01e545057bad2ea341b79b40cce2b2fc0d12
GPNaslund <135686203+GPNaslund@users.noreply.github.com>
2023-12-01 09:15:55 +0100

    GREEN

--- TaskManager/src/controller/ListTasksController.cs
+++ TaskManager/src/controller/ListTasksController.cs
@@ -7,17 +7,20 @@ namespace TaskManager.src.controller
     public class ListTasksController
     {
         private readonly View View;
+        private readonly ITaskService TaskService;
         public ListTasksController(View view, ITaskService service)
         {
             ArgumentNullException.ThrowIfNull(view);
             ArgumentNullException.ThrowIfNull(service);
             View = view;
+            TaskService = service;
         }
 
         public void Initialize()
         {
             View.DisplayHeader();
             View.DisplayMenu();
+            TaskService.ListTasksBy(ListByCommand.List_By_Due_Date);
         }
     }
 }
