0fa8eaf459cf1d799b5cb74ae057efd0fb91afe9
GPNaslund <135686203+GPNaslund@users.noreply.github.com>
2023-12-01 07:04:02 +0100

    GREEN

--- TaskManager/src/controller/AddTaskController.cs
+++ TaskManager/src/controller/AddTaskController.cs
@@ -4,9 +4,19 @@ namespace TaskManager.src.controller
 {
     public class AddTaskController
     {
+        private View View;
         public AddTaskController(View view)
         {
             ArgumentNullException.ThrowIfNull(view);
+            View = view;
+        }
+
+        public void Initialize()
+        {
+            string name = View.GetInput("Enter the name: ");
+            string description = View.GetInput("Enter the description: ");
+            string dueDateString = View.GetInput("Enter due date (yymmdd): ");
+            
         }
     }
 }
