b82a52663316a3bb7a37d9a45727af27d4c86298
GPNaslund <135686203+GPNaslund@users.noreply.github.com>
2023-12-01 07:03:57 +0100

    RED

--- TaskManager.Tests/UnitTests/controller/AddTaskControllerTest.cs
+++ TaskManager.Tests/UnitTests/controller/AddTaskControllerTest.cs
@@ -1,3 +1,5 @@
+using View = TaskManager.src.view.View;
+using Moq;
 using TaskManager.src.controller;
 
 namespace TaskManager.Tests.UnitTests.controller
@@ -7,9 +9,26 @@ namespace TaskManager.Tests.UnitTests.controller
         [Fact]
         public void Constructor_ShouldThrowArgumentNullException_OnNullValue()
         {
-            Assert.Throws<ArgumentNullException>(() => {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
                 AddTaskController Sut = new(null);
             });
+        }
+
+        [Fact]
+        public void Initialize_ShouldCollectDataForNewTask_Successfully()
+        {
+            Mock<View> MockView = new Mock<View>();
+            MockView.Setup(obj => obj.GetInput("Enter the name: ")).Returns("Test_Name");
+            MockView.Setup(obj => obj.GetInput("Enter the description: ")).Returns("Test_Description");
+            MockView.Setup(obj => obj.GetInput("Enter due date (yymmdd): ")).Returns(DateTime.Now.ToString("yyMMdd"));
+
+
+            AddTaskController Sut = new(MockView.Object);
+
+            Sut.Initialize();
+
+            MockView.Verify(obj => obj.GetInput(It.IsAny<string>()), Times.AtLeastOnce());
         }
     }
 }
