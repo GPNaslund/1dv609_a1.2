32d70f2f3d37012bd83a1f698029cf04e8af819a
GPNaslund <135686203+GPNaslund@users.noreply.github.com>
2023-12-01 13:16:16 +0100

    RED

--- TaskManager.Tests/UnitTests/controller/EditTaskControllerTest.cs
+++ TaskManager.Tests/UnitTests/controller/EditTaskControllerTest.cs
@@ -1,4 +1,7 @@
+using View = TaskManager.src.view.View;
 using TaskManager.src.controller;
+using Moq;
+using TaskManager.src.model;
 
 namespace TaskManager.Tests.UnitTests.controller
 {
@@ -10,6 +13,19 @@ namespace TaskManager.Tests.UnitTests.controller
             Assert.Throws<ArgumentNullException>(() => {
                 EditTaskController Sut = new(null, null);
             });
+        }
+
+        [Fact]
+        public void Initialize_ShouldAllowUserToSelectATaskToEdit()
+        {
+            Mock<View> MockView = new Mock<View>();
+            Mock<ITaskService> MockTaskService = new Mock<ITaskService>();
+
+            EditTaskController Sut = new EditTaskController(MockView.Object, MockTaskService.Object);
+
+            Sut.Initialize();
+
+            MockTaskService.Verify(obj => obj.GetAllTasks(), Times.AtLeastOnce());
         }
     }
 }
