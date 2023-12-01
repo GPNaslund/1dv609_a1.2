afa39f53402c1a32c3fd487de04c8842efbfaaf9
GPNaslund <135686203+GPNaslund@users.noreply.github.com>
2023-12-01 09:11:16 +0100

    GREEN

--- TaskManager/src/controller/ListTasksController.cs
+++ TaskManager/src/controller/ListTasksController.cs
@@ -6,15 +6,17 @@ namespace TaskManager.src.controller
 {
     public class ListTasksController
     {
+        private readonly View View;
         public ListTasksController(View view, ITaskService service)
         {
             ArgumentNullException.ThrowIfNull(view);
             ArgumentNullException.ThrowIfNull(service);
+            View = view;
         }
 
         public void Initialize()
         {
-            
+            View.DisplayMenu();
         }
     }
 }
