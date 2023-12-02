a487aa509d4b8e8bae8c9da85825c4400ec7f73f
GPNaslund <135686203+GPNaslund@users.noreply.github.com>
2023-12-02 19:12:18 +0100

    GREEN

--- TaskManager/src/controller/AppController.cs
+++ TaskManager/src/controller/AppController.cs
@@ -2,14 +2,16 @@ namespace TaskManager.src.controller
 {
     public class AppController 
     {
+        private readonly TaskFactory Factory;
         public AppController(TaskFactory factory)
         {
             ArgumentNullException.ThrowIfNull(factory);
+            Factory = factory;
         }
 
         public void Run()
         {
-            
+            Factory.Create_MainMenuController().Initialize();
         }
     }
 }
