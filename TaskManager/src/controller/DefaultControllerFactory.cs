746860581eb89edc66b328f7d5870c3f21a82ee6
GPNaslund <135686203+GPNaslund@users.noreply.github.com>
2023-12-03 18:28:36 +0100

    GREEN

--- TaskManager/src/controller/DefaultControllerFactory.cs
+++ TaskManager/src/controller/DefaultControllerFactory.cs
@@ -1,10 +1,15 @@
+using TaskManager.src.view;
+
 namespace TaskManager.src.controller
 {
     public class DefaultControllerFactory
     {
         public ExecutingController Create_MainMenuController()
         {
-            return null;
+            DefaultConsoleService ConsoleService = new();
+            ConsoleView View = new(ViewType.Main_Menu,ConsoleService);
+            MainMenuController mainMenuController = new(View);
+            return mainMenuController;
         }
     }
 }
