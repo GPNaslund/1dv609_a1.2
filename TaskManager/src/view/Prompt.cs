bc5aa139e2aaaa01b8e107602865dabf96399495
GPNaslund <135686203+GPNaslund@users.noreply.github.com>
2023-12-04 11:23:02 +0100

    REFACTOR

--- TaskManager/src/view/Prompt.cs
+++ TaskManager/src/view/Prompt.cs
@@ -2,10 +2,15 @@ namespace TaskManager.src.view
 {
     public class Prompt
     {
+        public string Name {get; set;}
+        public string Content {get; set;}
         public Prompt(string name, string content)
         {
             ArgumentNullException.ThrowIfNull(name);
             ArgumentNullException.ThrowIfNull(content);
+
+            Name = name;
+            Content = content;
         }
     }
 }
