90283ea3a7ddfb6c6c470533372999f4f19de432
GPNaslund <135686203+GPNaslund@users.noreply.github.com>
2023-12-04 11:42:26 +0100

    REFACTOR

--- TaskManager/src/view/ViewData.cs
+++ TaskManager/src/view/ViewData.cs
@@ -2,9 +2,33 @@ namespace TaskManager.src.view
 {
     public class ViewData
     {
+        private string _header;
+        public string Header {
+            get => _header;
+        }
+
+        private string[] _menu;
+        public string[] Menu 
+        {
+            get => _menu;
+        }
+
+        private Prompt[] _prompts;
+        public Prompt[] Prompts
+        {
+            get => _prompts;
+        }
         public ViewData(string header, string[] menu, Prompt[] prompts)
         {
-            throw new ArgumentNullException();
+            ArgumentNullException.ThrowIfNull(header);
+            ArgumentNullException.ThrowIfNull(menu);
+            ArgumentNullException.ThrowIfNull(prompts);
+
+            _header = header;
+            _menu = menu;
+            _prompts = prompts;
         }
+
+        
     }
 }
